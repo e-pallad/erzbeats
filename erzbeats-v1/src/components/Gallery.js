2c20ba2f6549445f142dc216f67c4a24c586ea47
Eric Pallad <e.pallad@pp-itservices.de>
2022-11-25 18:32:42 +0100

    fixing button issue in swiper

--- erzbeats-v1/src/components/Gallery.js
+++ erzbeats-v1/src/components/Gallery.js
@@ -6,7 +6,6 @@ import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/free-mode";
-import "swiper/css/navigation";
 
 // import required modules
 import { FreeMode, Navigation } from "swiper";
