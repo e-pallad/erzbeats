734dabdffec707301fd25dad5676eecf0defe905
Eric Pallad <e.pallad@pp-itservices.de>
2022-11-25 15:09:14 +0100

    splited up content

--- erzbeats-v1/src/App.js
+++ erzbeats-v1/src/App.js
@@ -1,9 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/style.css';
 
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
 import Header from './components/Header';
 import Hero from './components/Hero';
-import Content from './components/Content';
+import Lineup1 from './components/Lineup1';
+import Lineup2 from './components/Lineup2';
 import Footer from './components/Footer';
 
 function App() {
@@ -11,7 +15,16 @@ function App() {
     <div className="App">
       <Header />
       <Hero />
-      <Content />
+      <div className="content-section">
+        <Container>
+          <Row>
+            <Lineup1 />
+          </Row>
+          <Row>
+            <Lineup2 />
+          </Row>
+        </Container>
+      </div>
       <Footer />
     </div>
   );
