b506246efcc29cb394c3605c8b4d440a735e4365
Eric Pallad <e.pallad@pp-itservices.de>
2022-11-25 15:09:28 +0100

    added 2nd lineup variant

---
+++ erzbeats-v1/src/components/Lineup2.js
@@ -0,0 +1,90 @@
+import Col from "react-bootstrap/Col";
+
+export default function Lineup2() {
+    return (
+        <Col xs="12">
+            <div className="the-complete-lineup">
+                <div className="entry-title">
+                    <p>ERZbeat 2023</p>
+                    <h2>Lineup Variante 2</h2>
+                </div>
+                <div className="row the-complete-lineup-artists">
+                    <div className="col-6 col-md-4 col-lg-3 artist-single">
+                        <figure className="featured-image">
+                            <a href="#123">
+                                <img src="https://via.placeholder.com/255x295.png" alt=""/></a>
+                            <a href="#123" className="box-link">
+                                <img src="images/box.jpg" alt=""/></a>
+                        </figure>
+                        <h2>Miska Smith</h2>
+                    </div>
+                    <div className="col-6 col-md-4 col-lg-3 artist-single">
+                        <figure className="featured-image">
+                            <a href="#123">
+                                <img src="https://via.placeholder.com/255x295.png" alt=""/>
+                            </a>
+                            <a href="#123" className="box-link">
+                                <img src="images/box.jpg" alt=""/>
+                            </a>
+                        </figure>
+                        <h2>Hayley Down</h2>
+                    </div>
+                    <div className="col-6 col-md-4 col-lg-3 artist-single">
+                        <figure className="featured-image">
+                            <a href="#123">
+                                <img src="https://via.placeholder.com/255x295.png" alt=""/></a>
+                            <a href="#123" className="box-link">
+                                <img src="images/box.jpg" alt=""/></a>
+                        </figure>
+                        <h2>The Band Song</h2>
+                    </div>
+                    <div className="col-6 col-md-4 col-lg-3 artist-single">
+                        <figure className="featured-image">
+                            <a href="#123">
+                                <img src="https://via.placeholder.com/255x295.png" alt=""/></a>
+                            <a href="#123" className="box-link">
+                                <img src="images/box.jpg" alt=""/></a>
+                        </figure>
+                        <h2>Pink Machine</h2>
+                    </div>
+                    <div className="col-6 col-md-4 col-lg-3 artist-single">
+                        <figure className="featured-image">
+                            <a href="#123">
+                                <img src="https://via.placeholder.com/255x295.png" alt=""/></a>
+                            <a href="#123" className="box-link">
+                                <img src="images/box.jpg" alt=""/></a>
+                        </figure>
+                        <h2>Brasil Band</h2>
+                    </div>
+                    <div className="col-6 col-md-4 col-lg-3 artist-single">
+                        <figure className="featured-image">
+                            <a href="#123">
+                                <img src="https://via.placeholder.com/255x295.png" alt=""/></a>
+                            <a href="#123" className="box-link">
+                                <img src="images/box.jpg" alt=""/></a>
+                        </figure>
+                        <h2>Mickey</h2>
+                    </div>
+                    <div className="col-6 col-md-4 col-lg-3 artist-single">
+                        <figure className="featured-image">
+                            <a href="#123">
+                                <img src="https://via.placeholder.com/255x295.png" alt=""/></a>
+                            <a href="#123" className="box-link">
+                                <img src="images/box.jpg" alt=""/></a>
+                        </figure>
+                        <h2>DJ Girl</h2>
+                    </div>
+                    <div className="col-6 col-md-4 col-lg-3 artist-single">
+                        <figure className="featured-image">
+                            <a href="#123">
+                                <img src="https://via.placeholder.com/255x295.png" alt=""/></a>
+                            <a href="#123" className="box-link">
+                                <img src="images/box.jpg" alt=""/></a>
+                        </figure>
+                        <h2>Stan Smith</h2>
+                    </div>
+                </div>
+            </div>
+        </Col>
+    )
+}
