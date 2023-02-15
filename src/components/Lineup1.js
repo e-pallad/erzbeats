f4ca37dae54dadc72f28640dacd4f0e971c4edfe
Eric Pallad <e.pallad@pp-itservices.de>
2023-02-15 17:18:33 +0100

    added justin pollnik

--- src/components/Lineup1.js
+++ src/components/Lineup1.js
@@ -10,13 +10,13 @@ export default function Lineup1() {
                 </div>
                 <div className="lineup-artists">
                     <div className="lineup-artists-wrap flex flex-wrap">
-                        <figure className="featured-image" width={360} height={420}>
+                        <figure className="featured-image">
                             <a href="https://www.instagram.com/blvckcrowz/" target={"_blank"} rel={"noreferrer"}>
                                 <img src={require("../images/blvck-crows.png")} alt="Teaser: Blvck Crows" />
                             </a>
                         </figure>
                         <div className="lineup-artists-description">
-                            <div className="lineup-artists-description-container">
+                            <div className="lineup-artists-description-container blvck-crowz">
                                 <div className="entry-title">
                                     BLVCK CROWZ
                                 </div>
@@ -37,16 +37,16 @@ export default function Lineup1() {
                         <div className="lineup-artists-description">
                             <figure className="featured-image d-md-none">
                                 <a href="#somewhere" target={"_blank"} rel={"noreferrer"}>
-                                    <img src={require("../images/darley-jefferson.png")} alt="Teaser: Darley Jefferson" width={360} height={420} />
+                                    <img src={require("../images/darley-jefferson.png")} alt="Teaser: Darley Jefferson" />
                                 </a>
                             </figure>
-                            <div className="lineup-artists-description-container">
+                            <div className="lineup-artists-description-container darley-jefferson">
                                 <div className="entry-title">
                                     Darley Jefferson
                                 </div>
                                 <div className="entry-content">
                                     <p>
-                                        Quisque at erat eu libero consequat tempus. Quisque mole stie convallis tempus. Ut semper purus metus, a euismod sapien sodales ac. Duis viverra eleifend fermentum.
+                                        Der junge DJ aus Sachsen bietet euch nicht nur eine geile Show, sondern begleitet seit 2018 Millionen Menschen mit seiner Musik. Seine Mixtapes wurden mittlerweile über 50.000.000+ mal geklickt. Warum das so erfolgreich ist, wird er euch auf unserer Bühne zeigen.. please give a warm welcome!
                                     </p>
                                 </div>
                                 <div className="box-link">
@@ -58,18 +58,18 @@ export default function Lineup1() {
                         </div>
                         <figure className="featured-image d-none d-md-block">
                             <a href="#somewhere" target={"_blank"} rel={"noreferrer"}>
-                                <img src={require("../images/darley-jefferson.png")} alt="Teaser: Darley Jefferson" width={360} height={420} />
+                                <img src={require("../images/darley-jefferson.png")} alt="Teaser: Darley Jefferson" />
                             </a>
                         </figure>
                     </div>
                     <div className="lineup-artists-wrap flex flex-wrap">
                         <figure className="featured-image">
                             <a href="https://www.instagram.com/oscar_house_/" target={"_blank"} rel={"noreferrer"}>
-                                <img src={require("../images/oscar-house.png")} alt="Teaser: Oscar House" width={360} height={420} />
+                                <img src={require("../images/oscar-house.png")} alt="Teaser: Oscar House" />
                             </a>
                         </figure>
                         <div className="lineup-artists-description">
-                            <div className="lineup-artists-description-container">
+                            <div className="lineup-artists-description-container oscar-house">
                                 <div className="entry-title">
                                     Oscar House
                                 </div>
@@ -87,6 +87,39 @@ export default function Lineup1() {
                                 </div>
                             </div>
                         </div>
+                    </div>
+                    <div className="lineup-artists-wrap flex flex-wrap">
+                        <div className="lineup-artists-description">
+                            <figure className="featured-image d-md-none">
+                                <a href="#somewhere" target={"_blank"} rel={"noreferrer"}>
+                                    <img src={require("../images/justin-pollnik.jpg")} alt="Teaser: Justin Pollnik" />
+                                </a>
+                            </figure>
+                            <div className="lineup-artists-description-container justin-pollnik">
+                                <div className="entry-title">
+                                    Justin Pollnik
+                                </div>
+                                <div className="entry-content">
+                                    <p>
+                                        Justin Pollnik ist mit seinen 23 Jahren bereits einer der gefragtesten EDM-Acts in
+                                        Ostdeutschland und der wahrscheinlich bekannteste Harder Styles-Act im Osten der
+                                        Republik. Der Leipziger zieht die Massen mit, wenn er seine einzigartige Performance
+                                        abliefert - egal ob mit seinem deutschen Set aus Hardstyle & EDM oder seiner starken,
+                                        einmaligen Crowd Animation.
+                                    </p>
+                                </div>
+                                <div className="box-link">
+                                    <a href="#somewhere" target={"_blank"} rel={"noreferrer"}>
+                                        <img src={require("../images/box.png")} alt="" width={52} height={52} />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <figure className="featured-image d-none d-md-block">
+                            <a href="#somewhere" target={"_blank"} rel={"noreferrer"}>
+                                <img src={require("../images/justin-pollnik.jpg")} alt="Teaser: Justin Pollnik" />
+                            </a>
+                        </figure>
                     </div>
                 </div>
             </div>
