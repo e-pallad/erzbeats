aa94c5c48fe0d898f518e23b71f56ca8fb563e43
Eric Pallad <e.pallad@pp-itservices.de>
2023-02-15 16:37:14 +0100

    created new css classes for philosophie

--- src/components/Philosphie.js
+++ src/components/Philosphie.js
@@ -5,18 +5,18 @@ export default function Philosophie() {
         <Col xs="12">
             <Row>
                 <Col xs="12">
-                    <div className="lineup-artists-headline">
+                    <div className="philosophie-headline">
                         <div className="entry-title">
                             <p>ERZbeats 2023</p>
                             <h2>Unsere Philosophie</h2>
                         </div>
-                        <div className="lineup-artists">
-                            <div className="lineup-artists-wrap flex flex-wrap">
+                        <div className="philosophie">
+                            <div className="philosophie-wrap flex flex-wrap">
                                 <figure className="featured-image">
                                     <img src={require("../images/philosophie.jpg")} alt="Menschen genießen Musik" width={420} height={360} />
                                 </figure>
-                                <div className="lineup-artists-description">
-                                    <div className="lineup-artists-description-container">
+                                <div className="philosophie-description">
+                                    <div className="philosophie-description-container">
                                         <div className="entry-content">
                                             <p> 
                                                 Das Erzbeats Open Air, soll dich für einen Tag, aus dem üblichen Alltag herausholen. Wir kreieren an einem Tag unvergessliche Momente mit deinen Freunden und wollen dir genau das Feeling geben, was du sonst nur auf Festivals bekommst. Tanz mit uns bis in die Morgenstunden und lasse deinen Gefühlen dabei freien Lauf. 
