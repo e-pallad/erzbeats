57fd4715d76f4f69ee0895e887cd7a90e9833be0
Eric Pallad <e.pallad@pp-itservices.de>
2023-03-14 10:31:35 +0100

    minor fixes for anreise element

--- src/components/Anreise.js
+++ src/components/Anreise.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Iframe from 'react-iframe'
+import Iframe from 'react-iframe';
 import { Button, Row, Col } from 'react-bootstrap';
 
 export default class Anreise extends Component {
@@ -28,7 +28,7 @@ export default class Anreise extends Component {
                         <Col className="anreise-map">
                             {!accepted ? (
                                 <div className='map-disclaimer'>
-                                    <p>Mit einem Klick auf den Button akzeptierst du das Daten an und Cookies von Google gesendet und geladen werden.</p>
+                                    <p>Mit einem Klick auf den Button akzeptierst du das <a href="https://policies.google.com/technologies/retention?hl=de" target="_blank" rel="noreferrer">Daten</a> an und <a href="https://policies.google.com/technologies/cookies?hl=de" target="_blank" rel="noreferrer">Cookies</a> von Google gesendet und geladen werden.</p>
                                     <p>Alternativ findest du die Karte auch hier: <a href="https://www.google.com/maps/d/u/0/viewer?mid=1nPvFzdbPH-ih4n0rNWR-knrGp7kSthA&ll=50.8345697050548%2C13.435202250000033&z=19" target="_blank" rel="noreferrer">Google Maps</a></p>
                                     <Button variant="secondary" onClick={this.mapOpen}>Zeige Google Map</Button>
                                 </div>
