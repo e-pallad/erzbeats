aaa351adc9c1c2c60b59cbf556cfc50d52bca812
Eric Pallad <e.pallad@pp-itservices.de>
2023-03-14 14:15:31 +0100

    added iframe for tickets

--- src/pages/Tickets.js
+++ src/pages/Tickets.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import Container from "react-bootstrap/Container";
 import Iframe from 'react-iframe';
 
 export default function Tickets() {
+    useEffect(() => {
+        document.body.classList.add('ticket-page')
+        return () => {
+            document.body.classList.remove('ticket-page')
+        }
+    }, [])
+
     return (
-        <div className="content-tickets">
-            <Container>
+        <Container>
+            <div className='main-content'>
+                <div className='iframe-container'>
                 <Row>
                     <Col>
-                        <Iframe url="https://www.google.com/maps/d/embed?mid=1nPvFzdbPH-ih4n0rNWR-knrGp7kSthA&ehbc=2E312F"
-                            width="600px"
-                            height="450px"
+                        <Iframe url="https://erzbeats.ticket.io/"
+                            width="100%"
+                            height="1210px"
                             className="tickets-iframe"
                         />
                     </Col>
                 </Row>
-            </Container>
-        </div>
+                </div>
+            </div>
+        </Container>
     );
 }
