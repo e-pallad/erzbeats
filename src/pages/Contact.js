0a270b136ef67d369ebe75b003b154994d8f92c0
Eric Pallad <e.pallad@pp-itservices.de>
2023-03-14 13:04:51 +0100

    adapted contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 
+
 export default function Contact() {
+    useEffect(() => {
+        document.body.classList.add('contact-page')
+        return () => {
+            document.body.classList.remove('contact-page')
+        }
+    }, [])
+
     const radios = ["Booking", "Frage zu Tickets", "Sonstiges"];
     const [formData, setFormData] = useState({
         name: '',
@@ -32,66 +40,73 @@ export default function Contact() {
     };
 
     return (
-        <div className="content-contact">
             <Container>
-                <Row className="mt-5">
-                    <Col xs={12} md={{ span: 8, offset: 2 }}>
-                        <h2>Hier kannst du uns erreichen</h2>
-                        <Form onSubmit={handleSubmit}>
-                            <Form.Group controlId="formName" className='form-group'>
-                                <Form.Label>Name</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    name="name"
-                                    placeholder="Dein Name"
-                                    value={formData.name}
-                                    onChange={handleInputChange}
-                                />
-                            </Form.Group>
-                            <Form.Group controlId="formEmail" className='form-group'>
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control
-                                    type="email"
-                                    name="email"
-                                    placeholder="name@example.com"
-                                    value={formData.email}
-                                    onChange={handleInputChange}
-                                />
-                            </Form.Group>
-                            <Form.Group controlId="formReason" className='form-group'>
-                                <Form.Label>Grund deiner Anfrage</Form.Label>
-                                {radios.map((radio) => (
-                                    <div key={`key-${radio}`}>
-                                        <Form.Check 
-                                            type="radio"
-                                            id={`id-${radio}`}
-                                            label={`${radio}`}
-                                            value={`${radio}`}
-                                            checked={formData.subject === `${radio}`}
-                                            onChange={handleSubjectChange}
-                                        />
-                                    </div>
-                                ))}
-                            </Form.Group>
-                            <Form.Group controlId="formMessage" className='form-group'>
-                                <Form.Label>Nachricht</Form.Label>
-                                <Form.Control
-                                    as="textarea"
-                                    rows="5"
-                                    name="message"
-                                    placeholder="Deine Nachricht..."
-                                    value={formData.message}
-                                    onChange={handleInputChange}
-                                />
-                            </Form.Group>
-                            <Button variant="dark" type="submit">
-                                Absenden
-                            </Button>
-                        </Form>
-                    </Col>
-                </Row>
+                <div className='main-content'>
+                    <div className='get-in-touch'>
+                        <div className='entry-header'>
+                            <div className="entry-title">
+                                <h2>Hier kannst du uns erreichen</h2>
+                            </div>
+                        </div>
+                        <div className='contact-form'>
+                            <Row className="mt-5">
+                                <Col xs={12} md={{ span: 8, offset: 2 }}>
+                                    <Form onSubmit={handleSubmit}>
+                                        <Form.Group controlId="formName" className='form-group'>
+                                            <Form.Label>Name</Form.Label>
+                                            <Form.Control
+                                                type="text"
+                                                name="name"
+                                                placeholder="Dein Name"
+                                                value={formData.name}
+                                                onChange={handleInputChange}
+                                            />
+                                        </Form.Group>
+                                        <Form.Group controlId="formEmail" className='form-group'>
+                                            <Form.Label>Email</Form.Label>
+                                            <Form.Control
+                                                type="email"
+                                                name="email"
+                                                placeholder="name@beispiel.com"
+                                                value={formData.email}
+                                                onChange={handleInputChange}
+                                            />
+                                        </Form.Group>
+                                        <Form.Group controlId="formReason" className='form-group'>
+                                            <Form.Label>Grund deiner Anfrage</Form.Label>
+                                            {radios.map((radio) => (
+                                                <div key={`key-${radio}`}>
+                                                    <Form.Check 
+                                                        type="radio"
+                                                        id={`id-${radio}`}
+                                                        label={`${radio}`}
+                                                        value={`${radio}`}
+                                                        checked={formData.subject === `${radio}`}
+                                                        onChange={handleSubjectChange}
+                                                    />
+                                                </div>
+                                            ))}
+                                        </Form.Group>
+                                        <Form.Group controlId="formMessage" className='form-group'>
+                                            <Form.Label>Nachricht</Form.Label>
+                                            <Form.Control
+                                                as="textarea"
+                                                rows="5"
+                                                name="message"
+                                                placeholder="Deine Nachricht..."
+                                                value={formData.message}
+                                                onChange={handleInputChange}
+                                            />
+                                        </Form.Group>
+                                        <Button variant="dark" type="submit">
+                                            Absenden
+                                        </Button>
+                                    </Form>
+                                </Col>
+                            </Row>
+                        </div>
+                    </div>
+                </div>
             </Container>
-        </div>
     );
-    
 }
